ca9cb54011f9b67624d945d65931c4e4e07f46dd
Ben Bakker <bbakker@deerfield.edu>
2017-12-01 11:25:10 -0500

    Just trying to make my first commit of a project in Xcode to Github.

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Ben Bakker on 12/1/17.
+//  Copyright © 2017 Ben Bakker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
